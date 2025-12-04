8524f66e39857d566db65287c624b0865764f0c0
ABisman1 <asherb40@nycstudents.net>
2025-12-04 11:45:27 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -11,13 +11,17 @@ root.geometry("300x150")
 
 # Create buttons
 red_button = Button(root, text="Red", background='red')
+yellow_button = Button(root, text="Yellow", background='yellow')
+green_button = Button(root, text="Green", background='green')
 
 #Add a label
-label = Label(root, text="CHANGE ME!")
+label = Label(root, text="this is a stoplight")
 
 # Place widgets in window (with pack function!)
 label.pack()
 red_button.pack()
+yellow_button.pack()
+green_button.pack()
 
 
 # Start the GUI event loop
