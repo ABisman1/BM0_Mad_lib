17d223d38354772fa281f341d82cc720c830d9e0
ABisman1 <asherb40@nycstudents.net>
2025-12-05 12:53:57 -0500

    Update stopLight.py

--- stopLight.py
+++ stopLight.py
@@ -13,17 +13,19 @@ root.geometry("300x150")
 red_button = Button(root, text="Red", background='red')
 yellow_button = Button(root, text="Yellow", background='yellow')
 green_button = Button(root, text="Green", background='green')
+#white_button = Button(root, text="Color of LIGHT")
 
 #text_box = T = Text(root)
 
 #Add a label
-#label = Label(root, text="this is a stoplight")
+label = Label(root, text="What color is the light!?")
 
 # Place widgets in window (with pack function!)
-red_button.grid(row=0,column=10,padx=10,pady=10)
-yellow_button.grid(row=0,column=11,padx=10,pady=10)
-green_button.grid(row=0,column=12,padx=10,pady=10)
-#label.pack()
+red_button.grid(row=0,column=1,padx=10,pady=10)
+yellow_button.grid(row=0,column=2,padx=10,pady=10)
+green_button.grid(row=0,column=3,padx=10,pady=10)
+#white_button.pack()
+label.grid(row=2,column=2,pacy=20)
 #text_box.pack()
 
 # Start the GUI event loop
